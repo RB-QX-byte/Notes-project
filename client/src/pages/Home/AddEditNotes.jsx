759b2ce71e7baa308154bb8364b594639048d794
RB-QX-byte <rachitborkar7@gmail.com>
2026-02-06 18:33:48 +0530

    Implement title and content state management with corresponding input fields.

--- client/src/pages/Home/AddEditNotes.jsx
+++ client/src/pages/Home/AddEditNotes.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddEditNotes = () => {
-  return (
-    <div>
-        <div className="flex-flex-col gap-2">
-            <label className="input-label">TITLE</label>
-            <input 
-                type="text"
-                className="text-2xl text-slate-950 outline-none"
-                placeholder="Go to Gym At 6pm"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)} />
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    return (
+        <div>
+            <div className="flex flex-col gap-2">
+                <label className="input-label">TITLE</label>
+                <input
+                    type="text"
+                    className="text-2xl text-slate-950 outline-none"
+                    placeholder="Go to Gym At 6pm"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)} />
+            </div>
+
+            <div className="flex flex-col gap-2 mt-4">
+                <label className="input-label">CONTENT</label>
+                <textarea
+                    type="text"
+                    className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
+                    placeholder="Content"
+                    rows={10}
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default AddEditNotes
