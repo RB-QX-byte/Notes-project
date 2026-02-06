66b23ad1ed18dace856fdf0b5a2e83af3c71d7f4
RB-QX-byte <rachitborkar7@gmail.com>
2026-02-06 17:56:05 +0530

    Added NoteCard, ProfileInfo, and SearchBar components, integrating them into Navbar and Home page with a new `getInitials` helper.

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import ProfileInfo from '../Cards/ProfileInfo.jsx'
+import SearchBar from '../SearchBar/SearchBar.jsx';
 
 const Navbar = () => {
-  return (
-    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow-lg">
-        <h2 className="text-xl font-medium text-black py-2">Notes</h2>
-        
-    </div>
-  )
+
+    const [searchQuery, setSearchQuery] = React.useState("");
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate("/login");
+    };
+
+    const handleSearch = () => { };
+
+    const handleClearSearch = () => {
+        setSearchQuery("");
+    };
+
+    return (
+        <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow-lg">
+            <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+
+            <SearchBar
+                value={searchQuery}
+                onChange={({ target }) => {
+                    setSearchQuery(target.value);
+                }}
+                handleSearch={handleSearch}
+                onClearSearch={handleClearSearch}
+            />
+
+            <ProfileInfo onLogout={onLogout} />
+        </div>
+    )
 }
 
 export default Navbar
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import Navbar from '../../components/Navbar/Navbar.jsx'
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <>
+            <Navbar />
+
+            <div className="container mx-auto ">
+                <NoteCard />
+            </div>
+        </>
+    )
 }
 
 export default Home
