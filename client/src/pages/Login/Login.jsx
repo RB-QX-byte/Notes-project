489bde34c403deb31e2dc2722e1a254f93e204ff
RB-QX-byte <rachitborkar7@gmail.com>
2026-02-06 16:47:46 +0530

    Added the password should be set and should be greater than 6 characters, used the util helper files for checking the password, and also added the error logic, as well.

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar.jsx'
 import { Link } from 'react-router-dom'
 import PasswordInput from '../../components/Input/PasswordInput.jsx'
+import { validateEmail } from '../../utils/helper.js'
 
 const Login = () => {
 
@@ -11,7 +12,32 @@ const Login = () => {
 
     const handleLogin = async (e) => {
         e.preventDefault();
-    }
+
+        if (!validateEmail(email)) {
+            setError("Please enter a valid email address");
+            return;
+        }
+
+
+        if (password.length < 6) {
+            setError("Password must be at least 6 characters long");
+            return;
+        }
+        else if (!password) {
+            setError("Password is required");
+            return;
+        }
+
+        setError("");
+
+        //Login APi Callbacks
+
+
+
+
+    };
+
+
 
     return (
         <>
@@ -23,9 +49,9 @@ const Login = () => {
 
                         <h4 className="text-2xl mb-7">Login</h4>
 
-                        <input 
-                            type="text" 
-                            placeholder="Email" 
+                        <input
+                            type="text"
+                            placeholder="Email"
                             className="input-box"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
@@ -35,6 +61,8 @@ const Login = () => {
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                         />
+
+                        {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
 
                         <button type="submit" className="btn-primary">Login</button>
 
